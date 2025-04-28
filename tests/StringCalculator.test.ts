36b3c141081ee061bba89cf7071ddfc512c9b2ec
priyanka246 <priyanka@mindwaveventures.com>
2025-04-28 19:53:03 +0530

    [Add]: test for two numbers separated by comma returns their sum

--- tests/StringCalculator.test.ts
+++ tests/StringCalculator.test.ts
@@ -10,3 +10,12 @@ test('empty string returns 0', () => {
 test('single number returns the number', () => {
     expect(add("5")).toBe(5);
 });
+
+// Test that two numbers separated by comma returns their sum
+test('two numbers separated by comma returns their sum', () => {
+    expect(add("7,2")).toBe(9);
+});
+
+test('multiple numbers separated by comma returns their sum', () => {
+    expect(add("9,4,3")).toBe(16);
+});
