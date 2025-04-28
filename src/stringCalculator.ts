e4b23af69cc6960a1675ce7bc463b5142097c6e0
priyanka246 <priyanka@mindwaveventures.com>
2025-04-28 20:03:25 +0530

    [Update]: add function to return sum of num seprated by comma or newline

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,7 +1,7 @@
 
 export function add(str: string): number {
     if (str === "") return 0;
-    const inputNumbers = str.split(",");
+    const inputNumbers = str.split(/[\n,]+/);
     return inputNumbers.reduce((sum, num) => sum + Number(num), 0);
 }
 
