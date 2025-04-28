d2eaffe551614b6f30bbad0256e927efd67c8fb1
priyanka246 <priyanka@mindwaveventures.com>
2025-04-28 19:39:32 +0530

    [Add]: test for single number returns the number

--- tests/StringCalculator.test.ts
+++ tests/StringCalculator.test.ts
@@ -5,3 +5,8 @@ import { add } from '../src/stringCalculator';
 test('empty string returns 0', () => {
     expect(add("")).toBe(0);
 });
+
+// Test that single number returns the number
+test('single number returns the number', () => {
+    expect(add("5")).toBe(5);
+});
